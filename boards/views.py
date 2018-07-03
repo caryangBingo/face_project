8f3edd0eac9efad4c01f960af4e4af408a29c744
caryangBingo <zhiyuan1005873@gmail.com>
2018-07-03 19:02:57 +0800

    添加models

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
+
+
+def home(request):
+    boards = Board.objects.all()
+    boards_name = list(s)
