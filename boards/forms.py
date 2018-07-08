daa6d9f6d3ea5915a581a9bd01b98821f938b407
caryangBingo <zhiyuan1005873@gmail.com>
2018-07-08 15:13:55 +0800

    修改表单

--- boards/forms.py
+++ boards/forms.py
@@ -4,15 +4,19 @@
 # @Date:   2018-07-06 14:46:08
 # @Software : Sublime Text3
 # @File Name:   forms.py
-# @Last Modified time: 2018-07-06 14:50:38
+# @Last Modified time: 2018-07-08 13:46:47
 
 from django import forms
 from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message=forms.CharField(widget=forms.Textarea(),max_length=4000)
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': 'What is on your  mind?'}),
+        max_length=4000,
+        help_text='The max length of the text is 4000.')
 
     class Meta:
-        model=Topic
-        fields=['subject','message']
+        model = Topic
+        fields = ['subject', 'message']
