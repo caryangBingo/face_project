9b076fd53e5503f4a40c5e240316e5283683b0d9
caryangBingo <zhiyuan1005873@gmail.com>
2018-07-09 18:34:44 +0800

    添加accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
+
+
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
